8f5aec99f81ab44ea70a7e69ff2499ae175e0bb2
Dmitrii <dmitriy.ya@protonmail.com>
2024-05-06 16:48:47 +0200

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const MOCK_URLS = [
   "https://utfs.io/f/ff5b0949-71cf-4809-903c-a24822e47977-1d.png",
   "https://utfs.io/f/4043c822-120f-4fc3-abad-6d128fda5c18-1e.jpeg",
@@ -12,10 +14,17 @@ const MOCK_IMAGES = [...MOCK_URLS, ...MOCK_URLS, ...MOCK_URLS].map(
   }),
 );
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48 p-4">
+            <div>{post.name}</div>
+          </div>
+        ))}
         {MOCK_IMAGES.map((image) => (
           <div key={image.id} className="w-48 p-4">
             <img src={image.url} alt="mock" />
